daa338cdb8887ca612880a11ed636f1457a35e92
zcd1997 <1021230407@qq.com>
2020-03-08 22:19:59 +0800

    登录功能完成

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,20 @@ module.exports = {
     parser: 'babel-eslint'
   },
   rules: {
+    // 'consistent-return': 0,//return 后面是否允许省略
+    'no-useless-return': "error",
+    'quotes': [1, "single"],//引号类型 `` "" ''
+    'space-before-function-paren': 0, //方法名后空格
+    'semi': [2, "always"],//语句强制分号结尾
+    // keyword 前后需要空格
+    'keyword-spacing': [
+      2,
+      {
+        "before": true,
+        "after": true,
+        "overrides": {}
+      }
+    ],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
